49cd9a739a482f715231b4e1f0f5731976d1a3d8
Naditya Prastia Andino <aditandino2019211@gmail.com>
2026-04-23 17:10:38 +0700

    Jobsheet 20: Deploy Vercell

--- src/pages/produk/[produk].tsx
+++ src/pages/produk/[produk].tsx
@@ -1,52 +1,43 @@
-import fetcher from "../../utils/swr/fetcher";
-import { useRouter } from "next/router";
-import useSWR from "swr";
-import DetailProduk from "../../views/DetailProduct";
-import { ProductType } from "../../types/Product.type";
+import DetailProduk from "@/views/DetailProduct";
+import { ProductType } from "@/types/Product.type";
 
 const HalamanProduk = ({ product }: { product: ProductType }) => {
-    // {digunakan client-side rendering}
-    // const { query } = useRouter();
-    // const { data, error, isLoading } = useSWR(`/api/products/${query.produk}`, fetcher);
-    //
-    // return (
-    //     <div>
-    //         <DetailProduk products={isLoading ? {} as any : data?.data} />
-    //     </div>
-    // );
-
-    return (
-        <div>
-            <DetailProduk products={product} />
-        </div>
-    );
+  return (
+    <div>
+      <DetailProduk products={product} />
+    </div>
+  );
 };
 
 export default HalamanProduk;
 
-// Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
 // {digunakan server-side rendering}
-// export async function getServerSideProps({ params }: { params: { produk: string } }) {
-//     const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
-//     const respone = await res.json();
-//     // console.log("Data produk yang diambil dari API:", respone);
-//     
-//     return {
-//         props: {
-//             product: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
-//         },
-//     };
-// }
+export async function getServerSideProps({
+  params,
+}: {
+  params: { produk: string };
+}) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/produk/${params?.produk}`
+  );
+  const response = await res.json();
 
-// {digunakan static-site generation}
+  return {
+    props: {
+      product: response.data,
+    },
+  };
+}
+
+// {digunakan static-site generation di-comment/dihapus untuk menghindari error deployment}
+/*
 export async function getStaticPaths() {
-    const res = await fetch('http://localhost:3000/api/products');
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
     const response = await res.json();
 
     const paths = response.data.map((product: ProductType) => ({
         params: { produk: product.id }
     }));
-    // console.log("Paths yang dihasilkan untuk produk:", paths); // Debugging: Tampilkan paths yang dihasilkan
     return {
         paths,
         fallback: false
@@ -54,14 +45,13 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }: { params: { produk: string } }) {
-    const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
-    // const response: ProductType[] = await res.json();
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/produk/${params?.produk}`);
     const response: { data: ProductType } = await res.json();
 
-    // console.log("Data produk yang diambil dari API:", response);
     return {
         props: {
             product: response.data,
         }
     };
 }
+*/
